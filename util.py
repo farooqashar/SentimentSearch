559ea2ab51fdc96c4eead309852f342a4e970011
Ashar Farooq <ashar744@gmail.com>
2025-05-02 02:50:00 -0400

    adding more complex emotion mappings

--- util.py
+++ util.py
@@ -3,5 +3,44 @@ complex_emotion_map = {
     "goofy": ["happy", "surprise", "neutral"],
     "silly": ["happy", "surprise"],
     "melancholy": ["sad", "neutral"],
-    "nostalgia": ["surprise", "neutral", "happy", "sad"]
+    "nostalgia": ["surprise", "neutral", "happy", "sad"],
+    "bittersweet": ["happy", "sad", "neutral"],
+    "anxious": ["fear", "surprise", "neutral"],
+    "content": ["happy", "neutral"],
+    "hopeful": ["happy", "surprise"],
+    "serene": ["neutral", "happy"],
+    "guilty": ["sad", "fear", "disgust"],
+    "ashamed": ["sad", "disgust", "neutral"],
+    "proud": ["happy", "neutral", "surprise"],
+    "affectionate": ["happy", "neutral"],
+    "lonely": ["sad", "neutral"],
+    "conflicted": ["neutral", "sad", "happy"],
+    "frustrated": ["angry", "sad", "neutral"],
+    "resentful": ["angry", "disgust"],
+    "startled": ["surprise", "fear"],
+    "relieved": ["happy", "neutral", "sad"],
+    "overwhelmed": ["fear", "surprise", "sad"],
+    "awkward": ["neutral", "fear", "surprise"],
+    "disappointed": ["sad", "neutral"],
+    "inspired": ["happy", "surprise", "neutral"],
+    "peaceful": ["happy", "neutral", "sad"],
+    "curious": ["surprise", "neutral", "happy"],
+    "bored": ["neutral", "sad"],
+    "playful": ["happy", "surprise"],
+    "grateful": ["happy", "neutral"],
+    "embarrassed": ["sad", "surprise", "disgust"],
+    "determined": ["angry", "neutral", "happy"]
+}
+
+# Synonyms map
+emotion_synonyms = {
+    "funny": "goofy",
+    "joyful": "happy",
+    "scared": "fear",
+    "gross": "disgust",
+    "mad": "angry",
+    "serious": "neutral",
+    "loneliness": "lonely",
+    "hope": "hopeful",
+    "peace": "peaceful"
 }
